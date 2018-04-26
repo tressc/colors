48ac05ca9c655b944c6d8f6d5b455a1cc4471ad7
Colin <tresscd@gmail.com>
2018-04-26 00:45:27 -0400

    Use xy coords over id

---
+++ first.js
@@ -0,0 +1,83 @@
+const setupPage = () => {
+  for (let i = 1; i < 26; i++) {
+    // const a = Math.floor(Math.random() * 256);
+    // const b = Math.floor(Math.random() * 256);
+    // const c = Math.floor(Math.random() * 256);
+    const a = 256;
+    const b = 256;
+    const c = 256;
+    $(".root").append($(
+      `<div
+        class='square'
+        id='${i}'
+        style='background-color:rgb(${a},${b},${c});'
+      ></div>`
+    ));
+  }
+};
+
+const timedColors = () => {
+  for (let i = 1; i < 26; i++) {
+    const time = Math.floor(Math.random() * 10000);
+    const colorChange = () => {
+      const a = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+      const c = Math.floor(Math.random() * 256);
+      $(`#${i}`)[0].style = (
+        `background-color: rgb(${a},${b},${c});`
+      );
+    };
+    window.setInterval(colorChange, time);
+  }
+};
+
+const mouseColors = () => {
+  $(".square").on("mouseover", (e) => {
+    const a = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    const c = Math.floor(Math.random() * 256);
+    e.currentTarget.style = (
+      `background-color:rgb(${a},${b},${c});`
+    );
+  });
+};
+
+const waveColors = () => {
+  const visited = new Set;
+  const a = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  const c = Math.floor(Math.random() * 256);
+  let i = 1;
+  const newWave = () => {
+    for (let j = 1; j < 26; j++) {
+      if (!visited.has(j)) {
+        if (j % 4 === i % 4 && j / 4 < i) {
+          visited.add(j);
+          $(`#${j}`)[0].style = (
+            `background-color: rgb(${a},${b},${c});`
+          );
+        } else if (i % 4 === 0 && j % 4 === 0 && j / 4 <= i) {
+          visited.add(j);
+          $(`#${j}`)[0].style = (
+            `background-color: rgb(${a},${b},${c});`
+          );
+        } else if (i === 5 && j === 21) {
+          visited.add(j);
+          $(`#${j}`)[0].style = (
+            `background-color: rgb(${a},${b},${c});`
+          );
+        }
+      }
+    }
+    i += 1;
+    if (i === 10) waveColors();
+  };
+    window.setInterval(newWave, 100);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  setupPage();
+  mouseColors();
+  timedColors();
+  waveColors();
+});
---
+++ second.js
@@ -0,0 +1,44 @@
+const setupPage = () => {
+  for (let i = 0; i < 25; i++) {
+    const $div = $("<div>");
+    $div.attr("id", i);
+    $div.data({row: Math.floor(i / 5), col: i % 5});
+    $div.addClass("square");
+    $(".root").append($div);
+  }
+};
+
+const changeColor = (id, color) => {
+  $(`#${id}`)[0].style = (
+    `background-color: rgb(${color[0]},${color[1]},${color[2]});`
+  );
+};
+
+const waveColor = () => {
+  const a = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  const c = Math.floor(Math.random() * 256);
+  const rgb = [a, b, c];
+  let i = 0;
+  const changed = new Set;
+  const wave = () => {
+    for (let j = 0; j < 25; j++) {
+      if (!changed.has(j)) {
+        const row = $(`#${j}`).data().row;
+        const col = $(`#${j}`).data().col;
+        if (row + col === i) {
+          changed.add(j);
+          changeColor(j, rgb);
+        }
+      }
+    }
+    if (i < 8) i += 1;
+  };
+  window.setInterval(wave, 100);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  setupPage();
+  waveColor();
+  window.setInterval(waveColor, 300);
+});
